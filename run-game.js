51a0d0f55725bae0bdb2cd5ed3c88019221c981b
tms910 <146007979+tms910@users.noreply.github.com>
2024-01-24 01:19:08 +0200

    Make some functions to pick choise and decide winner

--- run-game.js
+++ run-game.js
@@ -1,16 +1,60 @@
+for(let count=0; count<5; count++){
 let rock=Math.floor(Math.random()*100)+1, paper=Math.floor(Math.random()*100)+1, scissors=Math.floor(Math.random()*100)+1;
+getComputerChoise();
+let computerSelection=getComputerChoise();
+let playerSelection=prompt('Choose Rock, Paper or Scissors');
+console.log(playTime(computerSelection,playerSelection));
+
+
+
+
 function getComputerChoise(){
-    console.log(rock,paper,scissors);
     if(rock>paper && rock>scissors){
-        console.log('Rock');
+        return 'Rock';
     }
     else if(paper>rock && paper>scissors){
-        console.log('Paper');
+        return 'Paper';
     }
     else{
-        console.log('Scissors');
+        return 'Scissors';
     }
 }
 
-getComputerChoise();
+    function playTime(computerSelection,playerSelection){
+        if(computerSelection==='Scissors'){
+            if(playerSelection==='paper' || playerSelection==='Paper' || playerSelection==='PAPER'){
+                return "Scissors vs Paper, Computer won";
+            }
+            else if(playerSelection==='rock' || playerSelection==='Rock' || playerSelection==='ROCK'){
+                return "Scissors vs Rock, Player won";
+            }
+            else if(playerSelection==='scissors' || playerSelection==='Scissors' || playerSelection==='SCISSORS'){
+                return "Scissors vs Scissors, it's a tie ";
+            }
+        }
 
+        else if(computerSelection==='Paper'){
+            if(playerSelection==='rock' || playerSelection==='Rock' || playerSelection==='ROCK'){
+                return "Paper vs Rock, Computer won";
+            }
+            else if(playerSelection==='scissors' || playerSelection==='Scissors' || playerSelection==='SCISSORS'){
+                return "Paper vs Scissors, Player won";
+            }
+            else if(playerSelection==='paper' || playerSelection==='Paper' || playerSelection==='PAPER'){
+                return "Paper vs Paper, it 's a tie";
+            }
+        }
+
+        else if(computerSelection==='Rock'){
+            if(playerSelection==='scissors' || playerSelection==='Scissors' || playerSelection==='SCISSORS'){
+                return "Rock vs Scissors, Computer won";
+            }
+            else if(playerSelection==='paper' || playerSelection==='Paper' || playerSelection==='PAPER'){
+                return "Rock vs Paper, Player won";  
+            }
+            else if(playerSelection==='rock' || playerSelection==='Rock' || playerSelection==='ROCK'){
+                return "Rock vs Rock, it's a tie";
+            }
+        }
+    }
+}
