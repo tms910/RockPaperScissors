04669f31ab9f4db11ce5896520e23c7e8defe64b
tms910 <146007979+tms910@users.noreply.github.com>
2024-03-23 17:23:49 +0200

    Give function to three html buttons

--- run-game.js
+++ run-game.js
@@ -1,30 +1,52 @@
 /*playerWins-computerWins are used as a score counter*/
-let playerWins=0,computerWins=0;
+let playerWins=0,computerWins=0, playerSelection='';
+
+const   rock=document.querySelector('#rock'),
+        paper=document.querySelector('#paper'),
+        scissors=document.querySelector('#scissors'),
+        results=document.querySelector('.results'),
+        div=document.createElement('div');
+
+
+/*Add event while clicking game buttons */
+rock.addEventListener('click',(event)=>{
+    playerSelection='Rock';
+    game();
+})
+
+paper.addEventListener('click',(event)=>{
+    playerSelection='Paper';
+    game();
+})
+
+scissors.addEventListener('click',(event)=>{
+    playerSelection='Scissors';
+    game();
+})
 
 function game(){
-    /*Declare game values*/
-    for(let count=0; count<5; count++){
+    /*Declare computer selection*/
         let rock=Math.floor(Math.random()*100)+1, 
             paper=Math.floor(Math.random()*100)+1,
-            scissors=Math.floor(Math.random()*100)+1;
-        let computerSelection=getComputerChoise(), playerSelection=prompt('Choose Rock, Paper or Scissors');
+            scissors=Math.floor(Math.random()*100)+1,
+            computerSelection=getComputerChoise();
 
         /*Decide whoever won the round and save the score value,when it's a tie nothing saves and loop counter goes down by 1*/
-        if(playTime(playerSelection,computerSelection)!=="Tie"){
-            if(playTime(playerSelection,computerSelection)==="Computer won"){
-                computerWins++;
-                console.log(playerSelection,'vs',computerSelection,':Computer Won');
+        
+            if(playTime(playerSelection,computerSelection)!=="Tie"){
+                if(playTime(playerSelection,computerSelection)==="Computer won"){
+                    computerWins++;
+                    results.textContent='Player: '+playerSelection +' vs '+'PC: '+computerSelection;
+                }
+                else if(playTime(playerSelection,computerSelection)==="Player won"){
+                    playerWins++;
+                    results.textContent='Player: '+playerSelection +' vs '+'PC: '+computerSelection;
+                }
             }
-            else if(playTime(playerSelection,computerSelection)==="Player won"){
-                playerWins++;
-                console.log(playerSelection,'vs',computerSelection,':Player Won');
+            
+            else if(playTime(playerSelection,computerSelection)==="Tie"){
+                results.textContent='Player: '+playerSelection +' vs '+'PC: '+computerSelection;
             }
-        }
-        else if(playTime(playerSelection,computerSelection)==="Tie"){
-            alert("It's a tie, play again! ");
-            count--;
-        }
-    
 
 
         /*function getComputerChoise decides the string return value of getComputerChoise*/
@@ -44,49 +66,61 @@ function game(){
         /*function playTime will decide the return string value according to computerSelection and playerSelection*/
         function playTime(playerSelection,computerSelection){
             if(computerSelection==='Scissors'){
-                if(playerSelection==='paper' || playerSelection==='Paper' || playerSelection==='PAPER'){
+                if(playerSelection==='Paper'){
                     return "Computer won";
                 }
-                else if(playerSelection==='rock' || playerSelection==='Rock' || playerSelection==='ROCK'){
+                else if(playerSelection==='Rock'){
                     return "Player won";
                 }
-                else if(playerSelection==='scissors' || playerSelection==='Scissors' || playerSelection==='SCISSORS'){
+                else if(playerSelection==='Scissors'){
                     return "Tie";
                 }
             }
 
             else if(computerSelection==='Paper'){
-                if(playerSelection==='rock' || playerSelection==='Rock' || playerSelection==='ROCK'){
+                if(playerSelection==='Rock'){
                     return "Computer won";
                 }
-                else if(playerSelection==='scissors' || playerSelection==='Scissors' || playerSelection==='SCISSORS'){
+                else if(playerSelection==='Scissors'){
                     return "Player won";
                 }
-                else if(playerSelection==='paper' || playerSelection==='Paper' || playerSelection==='PAPER'){
+                else if(playerSelection==='Paper'){
                     return "Tie";
                 }
             }
 
             else if(computerSelection==='Rock'){
-                if(playerSelection==='scissors' || playerSelection==='Scissors' || playerSelection==='SCISSORS'){
+                if(playerSelection==='Scissors'){
                     return "Computer won";
                 }
-                else if(playerSelection==='paper' || playerSelection==='Paper' || playerSelection==='PAPER'){
+                else if(playerSelection==='Paper'){
                     return "Player won";  
                 }
-                else if(playerSelection==='rock' || playerSelection==='Rock' || playerSelection==='ROCK'){
+                else if(playerSelection==='Rock'){
                     return "Tie";
                 }
             }
         }
-    }
 
-        /*console.log prints game results on console and then prints the final winner*/
-        console.log('Player score:',playerWins,'\nComputer score:',computerWins);
-        if(playerWins>computerWins){
-            console.log('Player is the final winner!!!');
+        div.textContent='Player score= '+playerWins+' Computer score= '+computerWins;
+        results.appendChild(div);
+
+        if(playerWins==5 || computerWins==5){
+            const final=document.createElement('p')
+            const div_player=document.createElement('p');
+            const div_computer=document.createElement('div');
+            final.textContent='Final Score:';
+            div_player.textContent='Player score= '+playerWins;
+            div_computer.textContent='Computer score= '+computerWins;
+            div.appendChild(final);
+            final.appendChild(div_player);
+            div_player.appendChild(div_computer);
+            playerWins=0;
+            computerWins=0;
         }
-        else{
-            console.log('Computer dominated you! :)');
-        }   
+
+        console.log(playerWins);
+        console.log(computerWins);
 }
+
+       
